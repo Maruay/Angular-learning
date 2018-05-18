ee74145eea3b50287a53e8d571691fb89b0ef5a8
Maruay <maruay.wk@gmail.com>
2018-05-18 19:29:38 +0700

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UserComponent } from './components/user/user.component';
+import { AboutusComponent } from './components/aboutus/aboutus.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    AboutusComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/user/user.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class UserComponent implements OnInit {
+
+  private name:string;
+  private age:number;
+  private email:string;
+  
+  // Dictionary
+  private address:{
+    street:string,
+    city:string,
+    province:string,
+    postcode:string
+  }
+
+  private skills:string[];
+
+  constructor() { }
+
+  ngOnInit() {
+    this.name = "Maruay";
+    this.age = 25;
+    this.email = "maruay.wk@gmail.com";
+
+    this.address = {
+      street: "94 Sutthisan Road",
+      city: "Din Daeng",
+      province: "Bangkok",
+      postcode: "10400"
+    }
+
+    this.skills = ["Programming", "Database", "Language"];
+  }
+
+}
